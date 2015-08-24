7588447df0a0114be4a2cb3db0d07295a65c4ce6
n8ebel <nebel@vandals.uidaho.edu>
2015-08-24 08:12:12 -0700

    Added EventEndpoint tests Test
    Added tests for EventEndpoint to ensure EventService methods are being called correctly.

--- marveldroid/src/main/java/com/n8/marveldroid/Endpoints/EventEndpoint.java
+++ marveldroid/src/main/java/com/n8/marveldroid/Endpoints/EventEndpoint.java
@@ -1,5 +1,75 @@
 package com.n8.marveldroid.Endpoints;
 
-public class EventEndpoint extends BaseEndpoint {
+import android.support.annotation.NonNull;
 
+import com.n8.marveldroid.EntityModelObjects.Event;
+import com.n8.marveldroid.QueryParams.EventQueryParams;
+import com.n8.marveldroid.RequestServices.EventService;
+import com.n8.marveldroid.ServiceResponse;
+
+import retrofit.Callback;
+import rx.Observable;
+
+public class EventEndpoint extends BaseEndpoint {
+    private EventService eventService;
+
+    public EventEndpoint(@NonNull EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    public void getEvents(@NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEvents(@NonNull EventQueryParams queryParams) {
+        return null;
+    }
+
+    public void getEvent(int eventId, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEvent(int eventId) {
+        return null;
+    }
+
+    public void getEventsForCharacterId(int characterId, @NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEventsForCharacterId(int characterId, @NonNull EventQueryParams queryParams) {
+        return null;
+    }
+
+    public void getEventsForComicId(int comic, @NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEventsForComicId(int comicId, @NonNull EventQueryParams queryParams) {
+        return null;
+    }
+
+    public void getEventsForCreatorId(int creatorId, @NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEventsForCreatorId(int creatorId, @NonNull EventQueryParams queryParams) {
+        return null;
+    }
+
+    public void getEventsForSeriesId(int seriesId, @NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEventsForSeriesId(int seriesId, @NonNull EventQueryParams queryParams) {
+        return null;
+    }
+
+    public void getEventsForStoryId(int storyId, @NonNull EventQueryParams queryParams, Callback<ServiceResponse<Event>> callback) {
+
+    }
+
+    public Observable<ServiceResponse<Event>> getEventsForStoryId(int storyId, @NonNull EventQueryParams queryParams) {
+        return null;
+    }
 }
--- marveldroid/src/main/java/com/n8/marveldroid/QueryParams/EventQueryParams.java
+++ marveldroid/src/main/java/com/n8/marveldroid/QueryParams/EventQueryParams.java
@@ -1,5 +1,138 @@
 package com.n8.marveldroid.QueryParams;
 
+import com.n8.marveldroid.RequestSignature;
+
+import java.util.Date;
+import java.util.List;
+
 public class EventQueryParams extends BaseQueryParams {
 
+    public enum OrderBy {
+        Default(null),
+        Name("name"),
+        StartDate("startDate"),
+        ModifiedDate("modified"),
+        NameDescending("-name"),
+        StartDateDescending("-startDate"),
+        ModifiedDateDescending("-modified");
+
+        private String mValue;
+
+        OrderBy(String value){
+            this.mValue = value;
+        }
+
+        public String getValue(){
+            return this.mValue;
+        }
+    }
+
+    private String name;
+    private String nameStartsWith;
+
+    private Date modifiedSince;
+
+    private List<Integer> creators;
+    private List<Integer> characters;
+    private List<Integer> series;
+    private List<Integer> comics;
+    private List<Integer> stories;
+
+    private OrderBy orderBy;
+
+    private int limit;
+    private int offset;
+
+    public EventQueryParams() {
+        super();
+        this.orderBy = OrderBy.Default;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameStartsWith() {
+        return nameStartsWith;
+    }
+
+    public void setNameStartsWith(String nameStartsWith) {
+        this.nameStartsWith = nameStartsWith;
+    }
+
+    public Date getModifiedSince() {
+        return modifiedSince;
+    }
+
+    public void setModifiedSince(Date modifiedSince) {
+        this.modifiedSince = modifiedSince;
+    }
+
+    public List<Integer> getCreators() {
+        return creators;
+    }
+
+    public void setCreators(List<Integer> creators) {
+        this.creators = creators;
+    }
+
+    public List<Integer> getSeries() {
+        return series;
+    }
+
+    public void setSeries(List<Integer> series) {
+        this.series = series;
+    }
+
+    public List<Integer> getComics() {
+        return comics;
+    }
+
+    public void setComics(List<Integer> comics) {
+        this.comics = comics;
+    }
+
+    public List<Integer> getStories() {
+        return stories;
+    }
+
+    public void setStories(List<Integer> stories) {
+        this.stories = stories;
+    }
+
+    public List<Integer> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<Integer> characters) {
+        this.characters = characters;
+    }
+
+    public OrderBy getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(OrderBy orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 }
