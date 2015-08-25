b437d90bf97a7ade2277352a98b575dcee57d4b1
n8ebel <nebel@vandals.uidaho.edu>
2015-08-25 08:57:16 -0700

    Implement SeriesEndpoint methods Feature
    Implement stubbed methods in SeriesEndpoint

--- marveldroid/src/main/java/com/n8/marveldroid/Endpoints/SeriesEndpoint.java
+++ marveldroid/src/main/java/com/n8/marveldroid/Endpoints/SeriesEndpoint.java
@@ -12,57 +12,239 @@ import rx.Observable;
 
 public class SeriesEndpoint extends BaseEndpoint {
 
-    private SeriesService seriesService;
+    private SeriesService mSeriesService;
 
     public SeriesEndpoint(@NonNull SeriesService seriesService) {
-        this.seriesService = seriesService;
+        mSeriesService = seriesService;
     }
 
     public void getSeriesForId(int seriesId, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeriesForId(seriesId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeriesForId(int seriesId) {
-        return null;
+        return mSeriesService.getSeriesForId(seriesId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature());
     }
 
     public void getSeries(@NonNull SeriesQueryParams queryParams, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeries(
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeries(@NonNull SeriesQueryParams queryParams) {
-        return null;
+        return mSeriesService.getSeries(
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset());
     }
 
     public void getSeriesForCharacterId(int characterId, @NonNull SeriesQueryParams queryParams, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeriesForCharacterId(
+                characterId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeriesForCharacterId(int characterId, @NonNull SeriesQueryParams queryParams) {
-        return null;
+        return mSeriesService.getSeriesForCharacterId(
+                characterId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset());
     }
 
     public void getSeriesForCreatorId(int creatorId, @NonNull SeriesQueryParams queryParams, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeriesForCreatorId(
+                creatorId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeriesForCreatorId(int creatorId, @NonNull SeriesQueryParams queryParams) {
-        return null;
+        return mSeriesService.getSeriesForCreatorId(
+                creatorId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset());
     }
 
     public void getSeriesForEventId(int eventId, @NonNull SeriesQueryParams queryParams, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeriesForEventId(
+                eventId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeriesForEventId(int eventId, @NonNull SeriesQueryParams queryParams) {
-        return null;
+        return mSeriesService.getSeriesForEventId(
+                eventId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getStories()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset());
     }
 
     public void getSeriesForStoryId(int storyId, @NonNull SeriesQueryParams queryParams, Callback<ServiceResponse<Series>> callback) {
-
+        mSeriesService.getSeriesForStoryId(
+                storyId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset(),
+                callback);
     }
 
     public Observable<ServiceResponse<Series>> getSeriesForStoryId(int storyId, @NonNull SeriesQueryParams queryParams) {
-        return null;
+        return mSeriesService.getSeriesForStoryId(
+                storyId,
+                String.valueOf(getTimestamp()),
+                getApiKey(),
+                getHashSignature(),
+                queryParams.getTitle(),
+                queryParams.getTitleStartsWith(),
+                queryParams.getStartYear(),
+                queryParams.getModifiedSince(),
+                getJoinedList(queryParams.getComics()),
+                getJoinedList(queryParams.getEvents()),
+                getJoinedList(queryParams.getCreators()),
+                getJoinedList(queryParams.getCharacters()),
+                queryParams.getSeriesType().getValue(),
+                queryParams.getContains().getValue(),
+                queryParams.getOrderBy().getValue(),
+                queryParams.getLimit(),
+                queryParams.getOffset());
     }
 }
