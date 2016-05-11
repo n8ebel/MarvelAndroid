0ddf41c9b1ebca74092742085da00a55756d7791
Nate Ebel <n8ebel@gmail.com>
2016-05-10 21:37:24 -0700

    Updated retrofit to 2.0

--- marvel_android/src/main/java/com/n8/marveldroid/RequestServices/RxEventService.java
+++ marvel_android/src/main/java/com/n8/marveldroid/RequestServices/RxEventService.java
@@ -5,39 +5,40 @@ import com.n8.marveldroid.RequestResponse;
 
 import java.util.Date;
 
-
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import rx.Observable;
 
-public interface EventService {
+
+public interface RxEventService {
 
     @GET("/v1/public/events")
-    RequestResponse<Event> getEvents(
-        @Query("ts") String timestamp,
-        @Query("apikey") String apikey,
-        @Query("hash") String hashSignature,
-        @Query("name") String name,
-        @Query("nameStartsWith") String nameStartsWith,
-        @Query("modifiedSince") Date modifiedSince,
-        @Query("creators") String creators,
-        @Query("characters") String characters,
-        @Query("series") String series,
-        @Query("comics") String comics,
-        @Query("stories") String stories,
-        @Query("orderBy") String orderBy,
-        @Query("limit") int limit,
-        @Query("offset") int offset);
+    Observable<RequestResponse<Event>> getEvents(
+            @Query("ts") String timestamp,
+            @Query("apikey") String apikey,
+            @Query("hash") String hashSignature,
+            @Query("name") String name,
+            @Query("nameStartsWith") String nameStartsWith,
+            @Query("modifiedSince") Date modifiedSince,
+            @Query("creators") String creators,
+            @Query("characters") String characters,
+            @Query("series") String series,
+            @Query("comics") String comics,
+            @Query("stories") String stories,
+            @Query("orderBy") String orderBy,
+            @Query("limit") int limit,
+            @Query("offset") int offset);
 
     @GET("/v1/public/events/{eventId}")
-    RequestResponse<Event> getEventForId(
-        @Path("eventId") int eventId,
-        @Query("ts") String timestamp,
-        @Query("apikey") String apikey,
-        @Query("hash") String hashSignature);
+    Observable<RequestResponse<Event>> getEventForId(
+            @Path("eventId") int eventId,
+            @Query("ts") String timestamp,
+            @Query("apikey") String apikey,
+            @Query("hash") String hashSignature);
 
     @GET("/v1/public/characters/{characterId}/events")
-    RequestResponse<Event> getEventsForCharacterId(
+    Observable<RequestResponse<Event>> getEventsForCharacterId(
             @Path("characterId") int characterId,
             @Query("ts") String timestamp,
             @Query("apikey") String apikey,
@@ -54,7 +55,7 @@ public interface EventService {
             @Query("offset") int offset);
 
     @GET("/v1/public/comics/{comicId}/events")
-    RequestResponse<Event> getEventsForComicId(
+    Observable<RequestResponse<Event>> getEventsForComicId(
             @Path("comicId") int comicId,
             @Query("ts") String timestamp,
             @Query("apikey") String apikey,
@@ -71,7 +72,7 @@ public interface EventService {
             @Query("offset") int offset);
 
     @GET("/v1/public/creators/{creatorId}/events")
-    RequestResponse<Event> getEventsForCreatorId(
+    Observable<RequestResponse<Event>> getEventsForCreatorId(
             @Path("creatorId") int creatorId,
             @Query("ts") String timestamp,
             @Query("apikey") String apikey,
@@ -88,7 +89,7 @@ public interface EventService {
             @Query("offset") int offset);
 
     @GET("/v1/public/series/{seriesId}/events")
-    RequestResponse<Event> getEventsForSeriesId(
+    Observable<RequestResponse<Event>> getEventsForSeriesId(
             @Path("seriesId") int seriesId,
             @Query("ts") String timestamp,
             @Query("apikey") String apikey,
@@ -105,7 +106,7 @@ public interface EventService {
             @Query("offset") int offset);
 
     @GET("/v1/public/stories/{storyId}/events")
-    RequestResponse<Event> getEventsForStoryId(
+    Observable<RequestResponse<Event>> getEventsForStoryId(
             @Path("storyId") int storyId,
             @Query("ts") String timestamp,
             @Query("apikey") String apikey,
@@ -120,4 +121,5 @@ public interface EventService {
             @Query("orderBy") String orderBy,
             @Query("limit") int limit,
             @Query("offset") int offset);
+
 }
